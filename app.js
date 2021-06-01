57259a003043edfbbdfa210c11019c93e4f55971
rafaelarango <arangoramirezrafael@gmail.com>
2021-05-31 20:37:40 -0500

    api-rest get

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const app = express();
 
+const usuarios = require('./data')
+
+const port = 3000;
 
 
 
 // get
-
-app.get('/'), (req, res) => {
+app.get('/', (req, res) => {
     res.send("Hello");
-}
+})
+
+// get ruta user
+app.get('/user', (req, res) => {
+    res.json(usuarios)
+
+})
 
 
 
 // configurar servidor
-app.listen(app.get(3000), () => {
-    console.log('Respuesta del servidor ');
-})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+
+ 
---
+++ data.js
@@ -0,0 +1,26 @@
+const usuarios = [
+
+    {
+        id : 1,
+        nombre :"Carlos",
+        apellido : "apellido",
+        municipio : "rionegro",
+    },
+
+    {
+        id : 2,
+        nombre : "Mabel",
+        apellido : "apellido",
+        municipio : "medellin",
+    },
+
+    {
+        id : 3,
+        nombre : "Yuli",
+        apellido : "apellido",
+        municipio : "Envigado",
+    }
+
+]
+
+module.exports = usuarios;
